6328d3ee15beb17bcc0b22fa6fdbf0c59f49afd9
Mohammed <m.alsami@hotmail.com>
2022-03-22 17:56:29 +0000

    basic shell and styling

---
+++ app.js
@@ -0,0 +1,15 @@
+// Gameboard interface
+
+const Gameboard = (()=>{
+    const x = "x";
+    const o = "o"
+    let gameboard = [[x,o,x],[o,x,o],[x,o,o]];
+
+})
+
+
+// Game Logic
+
+const GameLogic = (()=>{
+    
+})
