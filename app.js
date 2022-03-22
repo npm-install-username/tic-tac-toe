b3e7f1c44a83c72f366b5ab262a1c7778693d586
Mohammed <m.alsami@hotmail.com>
2022-03-22 18:27:35 +0000

    grid now displays game board

--- app.js
+++ app.js
@@ -5,11 +5,30 @@ const Gameboard = (()=>{
     const o = "o"
     let gameboard = [[x,o,x],[o,x,o],[x,o,o]];
 
-})
+    const paintGameBoard = function(gameboard) {
+        gameboard = gameboard.flat();
+        console.log(gameboard);
+        let gridCellArrary = document.querySelectorAll('grid-cell')
+        for (let i in gameboard){
+            console.log(i)
+            let gridCellTarget = document.getElementById(i);
+            console.log(gridCellTarget)
+            gridCellTarget.innerText = gameboard[i];
+        };
+    }
+    return {
+        gameboard,
+        paintGameBoard,
+        
+    };
+})();
+
+
+Gameboard.paintGameBoard(Gameboard.gameboard)
 
 
 // Game Logic
 
 const GameLogic = (()=>{
-    
+
 })
